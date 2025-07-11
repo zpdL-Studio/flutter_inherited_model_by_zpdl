ea2841dc7fad3fc3d270a5b4ddee061dff48808b
Kyunghwan Kim <kyunghwan.kim@motov.co.kr>
2025-07-11 13:35:50 +0900

    FlutterInheritedModel annotation

--- flutter_inherited_model/lib/flutter_inherited_model.dart
+++ flutter_inherited_model/lib/flutter_inherited_model.dart
@@ -1,5 +1,5 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+final class FlutterInheritedModel {
+  const FlutterInheritedModel();
 }
+
+const flutterInheritedModel = FlutterInheritedModel();
